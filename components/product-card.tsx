fe89f00efc220603c0e927748e10470575c10b37
remant <paudelremant45@gmail.com>
2025-06-28 23:11:29 +0545

    feat: add ProductCard and Badge components for product display

---
+++ components/product-card.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Eye } from "lucide-react";
+import { Badge } from "./ui/badge";
+import { useRouter } from "next/navigation";
+
+interface Product {
+  id: number;
+  name: string;
+  short_desc: string;
+  long_desc: string;
+  category: string;
+  sub_category: string;
+  image: string;
+  price: string;
+  stock: number;
+}
+interface ProductCardProps {
+  product: Product;
+}
+
+export function ProductCard({ product }: ProductCardProps) {
+  const router = useRouter();
+
+  const handleViewDetails = (id: number) => {
+    router.push(`/products/${id}`);
+  };
+  return (
+    <Card className="border-1 border-gray-200 pt-0 pb-2 md:pb-4  shadow-none">
+      <Image
+        src={product.image}
+        alt={product.name || "Product Image"}
+        width={300}
+        height={200}
+        className="object-cover w-full h-[240px] md:h-[350px] group-hover:scale-105 transition-transform duration-200 rounded-tl-xl rounded-tr-xl"
+      />
+      <CardContent className="p-4">
+        <div className="mb-2">
+          <Badge variant="secondary" className="text-xs">
+            {product.category}
+          </Badge>
+        </div>
+        <h3 className="font-semibold text-lg mb-2 hover:text-primary transition-colors line-clamp-2">
+          {product.name}
+        </h3>
+        <div className="flex items-center justify-between">
+          <span className="text-2xl font-bold">${product.price}</span>
+          <Badge variant={product?.stock > 0 ? "default" : "destructive"}>
+            {product?.stock > 0
+              ? `${product?.stock || 0} in stock`
+              : "Out of stock"}
+          </Badge>
+        </div>
+      </CardContent>
+      <CardFooter className="p-4 pt-0">
+        <Button
+          className="w-full"
+          onClick={() => handleViewDetails(product.id)}
+        >
+          <Eye className="mr-2 h-4 w-4" />
+          View Details
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
