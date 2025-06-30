9e53a0dd6d7cfca83b52a55eb0bebd715c899891
remant <paudelremant45@gmail.com>
2025-06-30 23:37:25 +0545

    feat: update image domain in next.config.ts to allow images from pics.craiyon.com

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["www.twtf.org.uk"],
+    domains: ["pics.craiyon.com"],
   },
 };
 
