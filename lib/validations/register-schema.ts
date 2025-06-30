821365894aff01cbfc03bc5b07ac69bb8d4a8aad
remant <paudelremant45@gmail.com>
2025-06-30 21:45:45 +0545

    feat: update register schema to include role and remove confirm password validation

--- lib/validations/register-schema.ts
+++ lib/validations/register-schema.ts
@@ -1,22 +1,17 @@
 import { z } from "zod";
 
-export const registerSchema = z
-  .object({
-    username: z.string().nonempty("Username is required"),
-    email: z.string().email("Invalid email address"),
-    password: z
-      .string()
-      .min(8, "Password must be at least 8 characters long")
-      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-      .regex(
-        /[!@#$%^&*(),.?":{}|<>]/,
-        "Password must contain at least one special character"
-      ),
-    confirmPassword: z.string().nonempty("Please confirm your password"),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Oops password dones not match!",
-    path: ["confirmPassword"],
-  });
+export const registerSchema = z.object({
+  username: z.string().nonempty("Username is required"),
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      "Password must contain at least one special character"
+    ),
+  role: z.enum(["USER", "ADMIN"]).optional(),
+});
 
 export type RegisterSchemaType = z.infer<typeof registerSchema>;
