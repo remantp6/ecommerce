407326263cc577b515439bb0967cbfe53f7b1b44
remant <paudelremant45@gmail.com>
2025-07-03 06:29:37 +0545

    feat: update image configuration to use remote patterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["pics.craiyon.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
   },
 };
 
